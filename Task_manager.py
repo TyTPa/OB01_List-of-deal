dd37de28f54a2a61b1ba09b15444cdc8556d7b63
TyTPa <papushina.tatyana@gmail.com>
2025-01-03 12:55:19 +0300

    output correction
    Исправлена ошибка в методе output

--- Task_manager.py
+++ Task_manager.py
@@ -12,23 +12,24 @@ class Task():
  def mark(self):
     self.status = 'Выполнено'
  def output(self):
-    if self.name=='Не выполнено':
+    if self.status=='Не выполнено':
         print(f' {self.name}  : Срок выполнения {self.deadline}')
  def info(self):
     print(f'Описание задачи - {self.name}')
     print(f'Статус задачи - {self.status}')
     print(f'Cрок выполнения- {self.deadline}')
 
-class Task_manager():
+class Task_Manager():
     def __init__(self):
         self.tasks = []
 
     def add_task(self):
         name = input('Введите описание задачи: ')
         deadline = input('Введите срок выполнения данной задачи: ')
-        task = Task(name, deadline)
+        status ='Не выполнено'
+        task = Task(name, deadline, status)
         self.tasks.append(task)
-        print(f'Создана новая задача: {task.name}')
+#        print(f'Создана новая задача: {task.name}')
 
     def mark_task(self, task_index):
         if 0 <= task_index < len(self.tasks):
@@ -41,6 +42,12 @@ class Task_manager():
         print('Текущие (не выполненные) задачи:')
         for task in self.tasks:
             task.output()
+task_manager=Task_Manager()
+for i in range (2):
+    task_manager.add_task()
+# Указываем индекс задачи для отметки как выполненной
+task_index_to_mark = int(input('Введите индекс задачи для отметки как выполненной (0 или 1): '))
+task_manager.mark_task(task_index_to_mark)
 
-
-task1 = Task('Полить цветы','03/01/25','Не выполнено')
+# Выводим текущие задачи
+task_manager.output_current_tasks()
