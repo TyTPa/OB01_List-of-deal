8227c21af98daf78a6bfffdac92347375878dae1
TyTPa <papushina.tatyana@gmail.com>
2025-01-09 20:08:55 +0300

    корректировка для использования get и set для класса Users, делаем его переменные защищенными

--- Ob2_Home_task.py
+++ Ob2_Home_task.py
@@ -7,26 +7,33 @@
 class Users():
 # Этот класс должен инкапсулировать данные :
 # ID, имя и уровень доступа ('user' для обычных сотрудников).
-  def __init__(self, name, ID, level ='user'):
-    self.name = name
-    self.ID = ID
-    self.level = level
+  def __init__(self, name, ID):
+    self._name = name
+    self._ID = ID
+    self._level = 'user'
 
 
   def info(self):
-    print (f"{self.name} - имя")
-    print (f"{self.ID} - персональный код")
-    print (f"{self.level} - уровень допуска")
+    print (f"{self._name} - имя")
+    print (f"{self._ID} - персональный код")
+    print (f"{self._level} - уровень допуска")
 
+  def get_ID(self):
+       return self._ID
+  def get_name(self):
+       return self._name
+  def get_level(self):
+      return self._level
 
 class Admin(Users):
-  def __init__(self,name,ID, level="admin"):
-      super().__init__(name, ID, level)  # Вызов конструктора родительского класса
+  def __init__(self,name,ID):
+      super().__init__(name, ID)  # Вызов конструктора родительского класса
+      self._level ='admin'
       self.user = {}  # Словарь для хранения пользователей
 
-  def add_user(self,name,ID,level = 'user'):
-      new_user = Users(name, ID, level)  # Создаем нового пользователя
-      self.user[new_user.ID] = new_user  # Сохраняем пользователя в словаре
+  def add_user(self,name,ID):
+      new_user = Users(name, ID)  # Создаем нового пользователя
+      self.user[new_user._ID] = new_user  # Сохраняем пользователя в словаре
 
   def remove_user(self, ID):
       if ID in self.user:
@@ -40,8 +47,8 @@ admin1 = Admin('Петя', 1)
 admin2 = Admin('Слава', 2)
 print( admin1.info)
 
-admin1.add_user('Анна', 3, 1)
-admin1.add_user('Мелисса', 4, 1)
+admin1.add_user('Анна', 3)
+admin1.add_user('Мелисса', 4)
 
 admin1.remove_user(3)  # Удаляем пользователя Анна
 admin1.remove_user(5)  # Пытаемся удалить несуществующего пользователя
